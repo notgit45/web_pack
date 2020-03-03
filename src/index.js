b8f8e5082789371df66a627cd514000860220649
notgit45 <ser8584@ya.ru>
2020-03-03 22:34:27 +0700

    End chapter Assets

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './image1.svg';
-import Data from './data.xml';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
 
     return element;
 }
